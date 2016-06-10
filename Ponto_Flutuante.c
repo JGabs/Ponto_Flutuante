bfc3838edf3bd3ff525ea5061e6ffe20a387e523
Daniel Furtado <daniel.96.furtado@gmail.com>
2016-06-09 22:44:04 -0300

    1.0
    Ponto flutuante calculado, ainda não trata caso de estouro e logica
    confusa.

--- Ponto_Flutuante.c
+++ Ponto_Flutuante.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct floating_point {
+struct FloatingPoint {
     char signal;
-    char exponent[8];
-    char mantissa[23];
+    char exponent[9];//caractere extra por causa do \0
+    char mantissa[23];//caractere extra por causa do \0
 };
 
-
-struct floating_point toLeft(struct floating_point floating_point, char signal, char *string);//trata o caso onde vc deve andar com a virgula para esquerda
-struct floating_point toRight(struct floating_point floating_point, char signal, char *string);//trata o caso onde vc deve andar com a virgula para direita
+//o to Left e Rigth podem virar um funcao só ps: fazer direito fds
+struct FloatingPoint toLeft(struct FloatingPoint floating_point, char signal, char *string);//trata o caso onde vc deve andar com a virgula para esquerda
+struct FloatingPoint toRight(struct FloatingPoint floating_point, char signal, char *string);//trata o caso onde vc deve andar com a virgula para direita
 int verify(char *string);//verifica se o input é valido;
 
 int main() {
     char number[1024];
 	char *n=number;
-    struct floating_point floating_point;
+    struct FloatingPoint floating_point;
     printf("Informe o valor: ");
     scanf("%s*C", n);
 	floating_point.signal='0';
@@ -26,33 +26,97 @@ int main() {
 	}
 	switch (verify(n)){
 		case 0:floating_point=toLeft(floating_point,floating_point.signal,n);break;
-		case 1:toRight(floating_point,floating_point.signal,n);break;
+		case 1:floating_point=toRight(floating_point,floating_point.signal,n);break;
 		case 2:strcpy(floating_point.exponent,"00000000");
 			   strcpy(floating_point.mantissa,"00000000000000000000000000000000");break;
 		case 3:strcpy(floating_point.exponent,"11111111");
 			   strcpy(floating_point.mantissa,"00000000000000000000000000000000");break;
 		default:printf("ERROR.");return 0;
 	}
+	floating_point.exponent[8]='\0';//bugava sem ele
+	floating_point.mantissa[23]='\0';
     printf("Signal:%c\nExponent:%s\nMantissa:%s", floating_point.signal, floating_point.exponent, floating_point.mantissa);
     
     return 0;
 }
 
-struct floating_point toLeft(struct floating_point floating_point, char signal, char *string){//em construcao
-	int cont=0,i=0;
-	floating_point.signal=signal;
-	while(*string!='1') string++;
-	while(*(string+1)!=','){
+struct FloatingPoint toLeft(struct FloatingPoint floating_point, char signal, char *string){
+	int cont=0,i=0,j;
+	floating_point.signal=signal;//só repete o sinal pro retorno
+	while(*string!='1') string++;//ignora os zeros de numeros como 00001,
+	string++;
+	while(*string!=',' && *string!='\0'){//conta o expoente
+		if(cont<23)floating_point.mantissa[cont]=*string;
 		cont++;
+		string++;
 	}
+	if(*string==',') string++;//pula a ,
+	j=127+cont;//calcula o expoente
+	for (i=7;i>=0;i--){
+		if(j==0)floating_point.exponent[i]='0';
+		else {
+			floating_point.exponent[i]= j%2 + '0';
+			j=j/2;
+		}
+	}
+	while(cont<23 ){//preenche a mantissa
+		if(*string=='\0') {//caso o vetor acabe preeenche com 0
+			floating_point.mantissa[cont]='0';
+			cont++;
+		}
+		else {
+			floating_point.mantissa[cont]=*string;
+			cont++;
+			string++;
+		}
+	}
+		
+	return (floating_point);
 }
 
-struct floating_point toRight(struct floating_point floating_point, char signal, char *string) {
-	
+struct FloatingPoint toRight(struct FloatingPoint floating_point, char signal, char *string) {
+	int cont=1,j,i=0;
+	floating_point.signal=signal;
+	while(*string!=',') string++;//como nao há nehum 1 antes da virgula garantido pela verify posiciona o numero depois da virgula em casos de input como 00000,
+	string++;
+	while(*string!='1'){//conta o expoente
+		cont++;
+		string++;
+		if(cont==127){//caso nao formalizado
+			strcpy(floating_point.exponent,"00000000");
+			for (i=0;i<32;i++){
+				if(*string=='\0') floating_point.mantissa[i]='0';
+				else{
+					floating_point.mantissa[i]=*string;
+					string++;
+				}
+			}
+			return (floating_point);
+		}
+	}
+	j=127-cont;//calcula o expoente
+	for (i=7;i>=0;i--){
+		if(j==0)floating_point.exponent[i]='0';
+		else {
+			floating_point.exponent[i]= j%2 + '0';
+			j=j/2;
+		}
+	}
+	while(i<23){//calcula a mantissa
+		if(*string=='\0'){
+			floating_point.mantissa[i]='0';
+			i++;
+		}
+		else{
+			floating_point.mantissa[i]=*string;
+			i++;
+			string++;
+		}
+	}
+	return (floating_point);
 }
 
 int verify(char *string){
-	printf("\n%s\n",string);
 	int i=0,flag=0;
 	/*   flag status: 0 - s/ 1 e ,
 				   1 - s/ ,
@@ -61,8 +125,6 @@ int verify(char *string){
 				   4 - s/ 1   */
 	if (*string=='i' && *(string+1)=='n' && *(string+2)=='f') return 3;
 	while (*(string+i)!='\0') {
-		
-	printf("\n%c\n",*(string+i));
 		switch (*(string+i)) {
 			case ',':if (flag==4 || flag==3) return -2;
 					 if (flag==0) flag=4;
@@ -73,14 +135,13 @@ int verify(char *string){
 					 break;
 			default : if(*(string+i)!='0') return -1;
 		}
-		printf("\n\n\n	%d	\n\n\n",flag);
 		i++;
 	}
-	printf("\n%d\n",flag);
 	switch (flag){
 		case 0:return 2;
 		case 1:return 0;
 		case 2:return 1;
+		case 3:return 0;
 		case 4:return 2;
 		default :return -3;
 	}
